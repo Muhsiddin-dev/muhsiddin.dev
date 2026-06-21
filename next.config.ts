1522e5f48d258117493d392b2f8a0f6584149702
MUHSIN2011 <muhsinnazarov21@gmail.com>
2026-06-21 22:58:36 +0500

    Update Project

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'donishyob.com',
+        hostname: 'donishyob.vercel.app',
       },
       {
         protocol: 'https',
